e3831f4a28185b77e4ed21d43d5befd846e0a003
prodeka <protazertyuiop@gmail.com>
2024-07-04 10:09:33 -0700

    Executing files

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Ceci est un sun script bashsh pour envoyer une requete a un URL et affiche la taille en octet
-curl -s -w "%{size_download}\n" "$1"
+#Ceci est un script bashsh pour envoyer une requete a un URL et affiche la taille en octet
+curl -s "$1" | wc -c
 
