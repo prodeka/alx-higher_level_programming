961eadfaabf50cdc2738679178b249bb873ec7dc
Serge TABE DJATO <protazertyuiop@gmail.com>
2024-07-04 18:32:28 +0000

    New_commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Ceci est un script bashsh pour envoyer une requete a un URL et affiche la taille en octet
+#Ceci est un script bash pour envoyer une requete a un URL et affiche la taille en octet
 curl -s "$1" | wc -c
-
